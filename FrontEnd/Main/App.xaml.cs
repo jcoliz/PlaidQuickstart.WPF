8f7780d96318c8e9131860c9e666c919048c3144
James Coliz <jcoliz@outlook.com>
2023-12-24 15:10:06 -0800

    feat: Add CefSharp Window to project
    Fixes #1721

--- FrontEnd/Main/App.xaml.cs
+++ FrontEnd/Main/App.xaml.cs
@@ -1,12 +1,12 @@
-﻿using FrontEnd.Ui;
-using System.Windows;
+﻿using CefSharp;
+using CefSharp.Wpf;
+using FrontEnd.Ui;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using CefSharp.Wpf;
-using CefSharp;
 using System.IO;
+using System.Windows;
 
 namespace FrontEnd.Main;
 /// <summary>
@@ -48,6 +48,9 @@ public partial class App : Application
                 services.AddSingleton<MainWindow>();
                 services.AddSingleton<MainViewModel>();
                 services.Configure<UiSettings>(context.Configuration.GetSection(UiSettings.Section));
+
+                // Only used for Standalone client
+                services.AddSingleton<LinkSchemeHandlerFactory>();
             })
             .ConfigureLogging((context, logging) =>
             {
@@ -75,6 +78,17 @@ public partial class App : Application
             LogSeverity = LogSeverity.Info,
             LogFile = "console.log"
         };
+
+        // Register scheme handler to intercept calls to back end
+        // Only used when running as standalone client
+        var schemehandler = _host.Services.GetService<LinkSchemeHandlerFactory>();
+        settings.RegisterScheme(new CefCustomScheme
+        {
+            SchemeName = LinkSchemeHandlerFactory.Scheme,
+            SchemeHandlerFactory = schemehandler,
+            DomainName = LinkSchemeHandlerFactory.Host,
+            IsSecure = true //treated with the same security rules as those applied to "https" URLs
+        });
 
         if (!Cef.IsInitialized)
         {
---
+++ FrontEnd/Ui/LinkSchemeHandlerFactory.cs
@@ -0,0 +1,46 @@
+﻿using CefSharp;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Web;
+
+using Application = System.Windows.Application;
+
+namespace FrontEnd.Ui;
+
+/// <summary>
+/// Scheme handler to intercept CefSharp links, and provide local responses
+/// </summary>
+/// <remarks>
+/// Should not be enabled in production. In production, just serve resources from app service.
+/// </remarks>
+/// <param name="linkResourceHandler">Service to be returned in the case of link API call</param>
+public class LinkSchemeHandlerFactory(ILogger<LinkSchemeHandlerFactory> logger) : ISchemeHandlerFactory
+{
+    public static string Scheme => "https";
+    public static string Host => "localhost";
+
+    public IResourceHandler Create(IBrowser browser, IFrame frame, string schemeName, IRequest request)
+    {
+        var uri = new Uri(request.Url);
+        var fileName = uri.AbsolutePath.ToLowerInvariant();
+
+        //
+        // Is this a static file?
+        //
+        // Static files are located in `wwwroot` directory, and added to the project as
+        // embedded resources
+        //
+        var resources = Application.ResourceAssembly.GetManifestResourceNames();
+        var found = resources.Where(x => x.EndsWith("wwwroot" + uri.AbsolutePath.Replace('/', '.')));
+        if (found.Any())
+        {
+            return ResourceHandler.FromStream(Application.ResourceAssembly.GetManifestResourceStream(found.First()));
+        }
+
+        logger.LogError("URL not found {url}", uri);
+        return ResourceHandler.ForErrorMessage("URL Not found", HttpStatusCode.NotFound);
+    }
+}
