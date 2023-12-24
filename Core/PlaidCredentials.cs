a6dd5625a9a34f2ff89ea729511b25883a32d8f1
James Coliz <jcoliz@outlook.com>
2023-12-24 15:22:02 -0800

    feat: Add Core.Models.PlaidCredentials
    Fixes #1724

---
+++ Core/PlaidCredentials.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Models;
+
+public class PlaidCredentials
+{
+	public static string SectionKey => "Plaid";
+	public string? LinkToken { get; set; }
+	public string? AccessToken { get; set; }
+	public string? ItemId { get; set; }
+	public string? Products { get; set; }
+	public string? CountryCodes { get; set; }
+    public string? Language { get; set; }
+}
