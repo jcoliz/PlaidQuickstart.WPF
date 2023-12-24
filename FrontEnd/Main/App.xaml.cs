48940394e707a183f97548a3c392471458f0cd2d
James Coliz <jcoliz@outlook.com>
2023-12-23 22:13:25 -0800

    feat: Add simple .NET Generic Host
    For #1703

--- FrontEnd/Main/App.xaml.cs
+++ FrontEnd/Main/App.xaml.cs
@@ -1,6 +1,6 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace FrontEnd.Main;
 /// <summary>
@@ -8,5 +8,32 @@ namespace FrontEnd.Main;
 /// </summary>
 public partial class App : Application
 {
+    private readonly IHost _host;
+
+    public App()
+    {
+        _host = new HostBuilder()
+            .ConfigureServices((context, services) =>
+            {
+                services.AddSingleton<MainWindow>();
+            })
+            .Build();
+    }
+
+    private async void Application_Startup(object sender, StartupEventArgs e)
+    {
+        await _host.StartAsync();
+
+        var mainWindow = _host.Services.GetService<MainWindow>();
+        mainWindow!.Show();
+    }
+
+    private async void Application_Exit(object sender, ExitEventArgs e)
+    {
+        using (_host)
+        {
+            await _host.StopAsync(TimeSpan.FromSeconds(5));
+        }
+    }
 }
 
