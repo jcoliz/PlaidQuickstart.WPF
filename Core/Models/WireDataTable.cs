be2a23d232aa370e97f461040273d65fe0e480da
James Coliz <jcoliz@outlook.com>
2023-12-24 19:18:53 -0800

    feat: Core Models & Providers for FetchClient
    Fixes #1717

---
+++ Core/Models/WireDataTable.cs
@@ -0,0 +1,36 @@
+﻿namespace Core.Models;
+
+/// <summary>
+/// Data table representation suitable for network (wire) transmission
+/// </summary>
+/// <remarks>
+/// This generic representation makes it easy to display data without worrying over 
+/// data types of each column
+/// </remarks>
+public class WireDataTable
+{
+    public Column[] Columns { get; set; } = [];
+
+    public Row[] Rows { get; set; } = [];
+
+    public WireDataTable() { }
+};
+
+public class Column
+{
+    public string Title { get; set; } = string.Empty;
+
+    public bool IsRight { get; set; }
+}
+
+public class Row
+{
+    public string[] Cells { get; set; } = [];
+
+    public Row() { }
+
+    public Row(params string[] cells) 
+    { 
+        Cells = cells;
+    }
+}
---
+++ Core/Providers/IFetchClient.cs
@@ -0,0 +1,12 @@
+﻿using Core.Models;
+
+namespace Core.Providers;
+
+/// <summary>
+/// Defines a service used to fetch data from the Plaid Service
+/// </summary>
+public interface IFetchClient
+{
+    Task<WireDataTable> Balance();
+    Task<WireDataTable> Transactions();
+}
