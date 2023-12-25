1d02e7130c5f8b7e9dfdccde059a07f1a32dc56d
James Coliz <jcoliz@outlook.com>
2023-12-24 19:32:33 -0800

    feat: Add fetching to MainViewModel
    Fixes #1739
    Fixes #1699: [User can] Fetch account balances from Plaid (RQ.1B1)
    Fixes #1700: [User can] Fetch account transactions from Plaid (RQ.1B2)

---
+++ FrontEnd/Ui/CommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Input;
+
+namespace FrontEnd.Ui;
+
+/// <summary>
+/// Creates instance of the command handler
+/// </summary>
+/// <param name="action">Action to be executed by the command</param>
+/// <param name="canExecute">A bolean property to containing current permissions to execute the command</param>
+public class CommandHandler(Action action, Func<bool> canExecute) : ICommand
+{
+    /// <summary>
+    /// Wires CanExecuteChanged event 
+    /// </summary>
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    /// <summary>
+    /// Forcess checking if execute is allowed
+    /// </summary>
+    /// <param name="parameter"></param>
+    /// <returns></returns>
+    public bool CanExecute(object? parameter)
+    {
+        return canExecute.Invoke();
+    }
+
+    public void Execute(object? parameter)
+    {
+        action();
+    }
+}
--- FrontEnd/Ui/MainViewModel.cs
+++ FrontEnd/Ui/MainViewModel.cs
@@ -1,4 +1,8 @@
 ﻿using System.ComponentModel;
+using System.Data;
+using System.Windows.Input;
+using Core.Models;
+using Core.Providers;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -8,7 +12,7 @@ namespace FrontEnd.Ui;
 /// View Model for the main interaction window
 /// </summary>
 /// <param name="fetchClient">Client we will use to fetch bank data</param>
-public class MainViewModel(ILogger<MainViewModel> logger, IOptions<UiSettings> settings)
+public class MainViewModel(ILogger<MainViewModel> logger, IOptions<UiSettings> settings, IFetchClient fetchClient) : INotifyPropertyChanged
 {
     /// <summary>
     /// Web location of the home page
@@ -31,5 +35,103 @@ public class MainViewModel(ILogger<MainViewModel> logger, IOptions<UiSettings> s
                 return result;
             }
         }
+
+    }
+
+    /// <summary>
+    /// Fires when something has changed
+    /// </summary>
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    /// <summary>
+    /// Initiate fetching of balances
+    /// </summary>
+    public ICommand FetchBalancesCommand => _FetchBalancesCommand ??= new CommandHandler(() => FetchBalances(), () => true);
+    private ICommand? _FetchBalancesCommand;
+
+    /// <summary>
+    /// Initiate fetching of transactions
+    /// </summary>
+    public ICommand FetchTransactionsCommand => _FetchTransactionsCommand ??= new CommandHandler(() => FetchTransactions(), () => true);
+    private ICommand? _FetchTransactionsCommand;
+
+    /// <summary>
+    /// Latest balances data from server
+    /// </summary>
+    public DataView? BalancesData
+    {
+        get; private set;
+    }
+
+    /// <summary>
+    /// Latest transactions data, fetched from server
+    /// </summary>
+    public DataView? TransactionsData
+    {
+        get; private set;
+    }
+
+    /// <summary>
+    /// Do the work of feteching balances
+    /// </summary>
+    protected async void FetchBalances()
+    {
+        try
+        {
+            var data = await fetchClient!.Balance();
+            var table = data.ToClientDataTable();
+            BalancesData = table.AsDataView();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "FetchBalances: FAILED");
+            BalancesData = null;
+        }
+        finally
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(BalancesData)));
+        }
+    }
+
+    /// <summary>
+    /// Do the work of fetching transactions
+    /// </summary>
+    protected async void FetchTransactions()
+    {
+        try
+        {
+            var data = await fetchClient!.Transactions();
+            var table = data.ToClientDataTable();
+            TransactionsData = table.AsDataView();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "FetchTransactions: FAILED");
+            TransactionsData = null;
+        }
+        finally
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TransactionsData)));
+        }
+    }
+}
+
+internal static class Extensions
+{
+    /// <summary>
+    /// Convert the data table we got over the network (wire) into the form that the
+    /// UI can display
+    /// </summary>
+    /// <param name="t">Source table</param>
+    /// <returns>Resulting table</returns>
+    public static DataTable ToClientDataTable(this WireDataTable t)
+    {
+        var table = new DataTable();
+        table.Columns.AddRange(t.Columns.Select(x => new DataColumn(x.Title)).ToArray());
+        foreach (var row in t.Rows)
+        {
+            table.Rows.Add(row.Cells);
+        }
+        return table;
     }
 }
